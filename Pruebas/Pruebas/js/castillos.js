2d6e9a390feab541da07db34b03b8ff185890bf6
angelruizmorales <93920185+angelruizmorales@users.noreply.github.com>
2023-05-10 09:37:48 +0200

    Add files via upload

---
+++ Pruebas/Pruebas/js/castillos.js
@@ -0,0 +1,23 @@
+var castillos={
+    "type": "FeatureCollection",
+    "generator": "overpass-turbo",
+    "copyright": "The data included in this document is from www.openstreetmap.org. The data is made available under ODbL.",
+    "timestamp": "2017-05-07T15:09:59Z",
+    "features": [
+      {
+        "type": "Feature",
+        "properties": {
+          "amenity": "drinking_water"
+        },
+        "geometry": {
+          "type": "Point",
+          "coordinates": [
+            36.720876,
+            -4.416964
+          ]
+        },
+        
+      },
+  
+    ]
+  }
